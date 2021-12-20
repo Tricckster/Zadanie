bd0990fb89bbeecc6391190360a24661bd29d103
Mad <1233@gmail.com>
2021-12-20 20:48:24 +0300

    print pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void work()
 {
 	/**
-    1) считать и запомнить числа
+    + 1) считать и запомнить числа
     2) вывести все пары чисел
     3) вместо пар вывести произведения
     4) проверить делимость произведений
@@ -17,8 +17,9 @@ void work()
 	vector<int> numbers(n, 0);
 	for (int i(0); i<n; i++)
 		cin >> numbers[i];
-	for (int i(0); i<n; i++)
-		cout << numbers[i] << " ";
+	for (int j(0); j<n; j++)
+		for (int i(j+1); i<n; i++)
+			cout << numbers[j] << " " << numbers[i] << endl;
 }
 
 int main(int argc, char** argv) {
