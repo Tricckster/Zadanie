1cdd8d116b4b708358fe40cc60e13829d35dd8f2
Mad <1233@gmail.com>
2021-12-20 21:51:47 +0300

    print max sum which %2 != 0

--- main.cpp
+++ main.cpp
@@ -10,17 +10,18 @@ void work()
     + 2) вывести все пары
     + 3) вместо пары вывести суммы
     + 4) проверить делимость суммы
-    5) вывести только наибольшую сумму
+    + 5) вывести только наибольшую сумму
 	*/
-	int n(0);
+	int n(0), max(-1);
 	cin >> n;
 	vector<int> numbers(n, 0);
 	for (int i(0); i<n; i++)
 		cin >> numbers[i];
 	for (int i(0); i<n; i++)
 		for (int j(i+1); j<n; j++)
-			if ((numbers[i] + numbers[j])%2 != 0)
-				cout << numbers[i] + numbers[j] << endl;
+			if ((numbers[i] + numbers[j])%2 != 0 and numbers[i] + numbers[j] > max)
+				max = numbers[i] + numbers[j];
+	cout << max << endl;
 }
 
 int main(int argc, char** argv)
@@ -28,4 +29,3 @@ int main(int argc, char** argv)
 	work();
 	return 0;
 }
-
