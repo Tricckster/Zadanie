d42c64c44b6964be006c104bdda0f1e02a638e5a
Mad <1233@gmail.com>
2021-12-20 21:09:33 +0300

    read and print numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 void  work()
 {
 	/**
-    1) считать и запомнить числа
+    + 1) считать и запомнить числа
     2) вывести все пары чисел
     3) вместо пар вывести произведения
     4) проверить делимость произведений
     5) посчиттать и выести коичество подходящих пар
-	*/	
+	*/
+	int n(0);
+	cin >> n;
+	vector<int> numbers(n, 0);
+	for (int i(0); i<n; i++)
+		cin >> numbers[i];
+	for (int i(0); i<n; i++)
+		cout << numbers[i] << " ";
 	
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+	work();
 	return 0;
 }
