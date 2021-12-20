c84f62f2ff72e6e0576824ea9a77acd84aa4afc3
Mad <1233@gmail.com>
2021-12-20 22:24:35 +0300

    read and print numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void work()
 {
 	/**
-    1) считать и запомнить числа
+    + 1) считать и запомнить числа
     2) найти наибольшее четное и наибольшее нечетное числа
     3) вывести сумму наибольших чисел
 	*/
+	int n(0);
+	cin >> n;
+	vector<int> numbers(n, 0);
+	for (int i(0); i<n; i++)
+		cin >> numbers[i];
+	for (int i(0); i<n; i++)
+		cout << numbers[i] << " ";
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
+	work();
 	return 0;
 }
+
