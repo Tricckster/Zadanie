d6833afe03a103c15c0c44de95a98336203b5cc8
Mad <1233@gmail.com>
2021-12-20 20:57:25 +0300

    print number of pairs which %14 == 0

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void work()
     + 4) проверить делимость произведений
     5) посчиттать и выести коичество подходящих пар
 	*/	
-	int n(0);
+	int n(0), otv(0);
 	cin >> n;
 	vector<int> numbers(n, 0);
 	for (int i(0); i<n; i++)
@@ -20,7 +20,8 @@ void work()
 	for (int j(0); j<n; j++)
 		for (int i(j+1); i<n; i++)
 			if ((numbers[j] * numbers[i])%14 == 0)
-				cout << numbers[j] * numbers[i] << endl;
+				otv++;
+	cout << otv << endl;
 }
 
 int main()
