3353e664d9bb559e64996c94f5d02db16a3fb542
Алексей <qwerty143928@gmail.com>
2021-12-21 13:59:04 +0300

    added a work plan

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-int main(int argc, char** argv) {
+void work()
+{
+    /*
+    1. считать, запомнить и вывести числа
+    2. посчитать значение при делении каждого из 3 элементов на 4 и сложить их, если получается целое число, то прибавить к otv 1
+    3. вывести значение otv
+    */
+}
+
+int main(int argc, char** argv)
+{
+    work();
 	return 0;
 }
