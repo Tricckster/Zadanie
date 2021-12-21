3a483ef6411ee8f68cd9e4b65e709e75142a5295
Алексей <qwerty143928@gmail.com>
2021-12-21 12:35:04 +0300

    read and print numbers

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,13 @@ void work()
 	3. проврить кратна ли сумма 4, если да то вывести
 	4. посчитать сколько таких троек которые кратны 4
 	*/
+	int n(0);
+	cin >> n;
+	vector<int> numbers(n, 0);
+	for (int i(0); i<n; i++)
+		cin >> numbers[i];
+	for (int i(0); i<n; i++)
+		cout << numbers[i] << " ";
 }
 
 int main(int argc, char** argv)
