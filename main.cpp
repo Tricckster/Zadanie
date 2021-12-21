c6f714a3d52c9ae7ee64af5c9bd5ac5421668cd8
Алексей <qwerty143928@gmail.com>
2021-12-21 14:00:18 +0300

    read and print numbers

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,13 @@ void work()
     2. посчитать значение при делении каждого из 3 элементов на 4 и сложить их, если получается целое число, то прибавить к otv 1
     3. вывести значение otv
     */
+    int n(0);
+	cin >> n;
+	vector<int> numbers(n, 0);
+	for (int i(0); i<n; i++)
+		cin >> numbers[i];
+	for (int i(0); i<n; i++)
+		cout << numbers[i] << " ";
 }
 
 int main(int argc, char** argv)
