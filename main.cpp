2c4cbc8506c8d2102d979f47201c2054d8c6db35
Алексей <qwerty143928@gmail.com>
2021-12-21 12:54:58 +0300

    print numbers of 3 elem sum if %4 == 0. Ineff work complete

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ void work()
 	+ 1. считать числа и вывести их
 	+ 2. посчитать сумму 3 чисел и вывести их
 	+ 3. проврить кратна ли сумма 4, если да то вывести
-	4. посчитать сколько таких троек которые кратны 4
+	+ 4. посчитать сколько таких троек которые кратны 4
 	*/
-	long long int n(0);
+	long long int n(0), cor(0);
 	cin >> n;
 	vector<long long int> numbers(n, 0);
 	for (long long int i(0); i<n; i++)
@@ -19,7 +19,8 @@ void work()
 		for (long long int j(i+1); j<n; j++)
 			for (long long int b(j+1); b<n; b++)
                 if ((numbers[i] + numbers[j] + numbers[b]) % 4 == 0)
-				cout << numbers[i] << " " << numbers[j] << " " << numbers[b] << " " << numbers[i] + numbers[j] + numbers[b] << endl;
+                    cor++;
+    cout << cor << endl;
 }
 
 int main(int argc, char** argv)
